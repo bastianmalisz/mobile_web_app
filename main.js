e15a9d8513fdbda50628f71d2e08ef1779267ed7
Sebastian Maliszewski <malisz654321@gmail.com>
2018-03-31 22:23:58 +0200

    Fundaments for app

---
+++ main.js
@@ -0,0 +1,43 @@
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
+        if (xDiff > 0) {
+            console.log("Swiped left");
+            if (document.querySelector(".sidenav").classList.contains("open")) {
+                document.querySelector(".sidenav").classList.remove("open");
+            }
+        } else {
+            console.log("Swiped right");
+            document.querySelector(".sidenav").classList.add("open");
+        }
+    } else {
+        if (yDiff > 0) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
